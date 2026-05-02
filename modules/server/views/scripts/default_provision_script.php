0adbf48f364652571aafcc240eb486d5187d1a74
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2026-05-02 18:08:33 +0200

    fix(server): removed `provision` project specific sudo rule

--- modules/server/views/scripts/default_provision_script.php
+++ modules/server/views/scripts/default_provision_script.php
@@ -32,7 +32,6 @@ $PROVISION_USER ALL=(ALL) NOPASSWD: /usr/bin/systemctl reload apache2
 $PROVISION_USER ALL=(ALL) NOPASSWD: /usr/bin/systemctl restart apache2
 $PROVISION_USER ALL=(ALL) NOPASSWD: /usr/bin/mkdir -p /var/www, /usr/bin/mkdir -p /var/www/*
 $PROVISION_USER ALL=(ALL) NOPASSWD: /usr/bin/install -d -m 0755 -o $PROVISION_USER -g $PROVISION_USER /var/www/releases/*
-$PROVISION_USER ALL=(ALL) NOPASSWD: /usr/bin/mv -Tf /tmp/provision_promote_* /var/www/*, /usr/bin/mv -Tf /tmp/provision_demote_* /var/www/*
 $PROVISION_USER ALL=(ALL) NOPASSWD: /usr/bin/chgrp -R www-data /var/www, /usr/bin/chgrp -R www-data /var/www/*
 $PROVISION_USER ALL=(ALL) NOPASSWD: /usr/bin/tee /etc/apache2/sites-available/*.conf
 $PROVISION_USER ALL=(ALL) NOPASSWD: /usr/bin/a2ensite *
